abe803b45541fa9bb930dbfe314c84497d062da0
Bohdan Polienko <mignatov756@gmail.com>
2021-02-25 18:30:32 +0200

    Updates

---
+++ precache-manifest.f7eee55e9e261cc1daae6bafe0905ebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29022400cdb66398ad652b2e468fdf65",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2410a24f34da72c23fe8",
+    "url": "./static/css/main.49e005fe.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2410a24f34da72c23fe8",
+    "url": "./static/js/main.49c614c2.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
